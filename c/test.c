3828f129d865a9c67c2bee239e98bdb389195b7b
davi2205 <danilo.davilla@gmail.com>
2022-09-13 00:23:30 -0300

    up

--- c/test.c
+++ c/test.c
@@ -1,24 +1,73 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct DVM_Token
+static size_t alignSize(size_t size, size_t alignment)
+{
+    size_t a = size % alignment;
+    return a == 0 ? size : size + (alignment - a);
+}
+
+typedef struct DVM_BufferMeta
+{
+    size_t refCount;
+} DVM_BufferMeta;
+
+typedef struct DVM_String
+{
+    size_t length;
+    char *buffer;
+} DVM_String;
+
+static void DVM_StringInit(const char *cStr, DVM_String *str)
+{
+    size_t len = strlen(cStr) + 1;
+    size_t metaOffset = alignSize(len, sizeof(DVM_BufferMeta));
+
+    str->length = len;
+    str->buffer = malloc(metaOffset + sizeof(DVM_BufferMeta));
+
+    memcpy(str->buffer, cStr, len);
+
+    DVM_BufferMeta *meta = (DVM_BufferMeta *)(str->buffer + metaOffset);
+    meta->refCount = 0;
+}
+
+static void DVM_StringDispose(DVM_String *str)
+{
+    size_t metaOffset = alignSize(str->length, sizeof(DVM_BufferMeta));
+    DVM_BufferMeta *meta = (DVM_BufferMeta *)(str->buffer + metaOffset);
+
+    if (meta->refCount > 0)
+    {
+        meta->refCount--;
+        return;
+    }
+
+    free(str->buffer);
+
+    str->length = 0;
+    str->buffer = NULL;
+}
+
+typedef struct DVM_Value
 {
     char type;
     union {
-        double asNumber;
-    } value;
-} DVM_Token;
-
-typedef struct DVM_Stream
-{
-    const char *current;
-    const char *end;
-} DVM_Stream;
-
-
+        DVM_String asString;
+    };
+} DVM_Value;
 
 int main()
 {
-    DVM_Token t;
-    t.value.asNumber;
+    DVM_String str;
+    DVM_StringInit("hell world", &str);
+
+    
+
+    printf("%s\n", (const char *)str.buffer);
+    printf("%li\n", sizeof(DVM_Value));
+
+    DVM_StringDispose(&str);
 }
