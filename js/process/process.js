57208db67bceb736bb3668ca8da69a9b1034c06d
Davi Villalva <davillal@gmail.com>
2026-08-14 09:07:44 -0300

    feat: enhance process manager with improved type handling and message broadcasting

--- js/process/process.js
+++ js/process/process.js
@@ -9,45 +9,79 @@ function createProcessManager() {
 
   // Types
   // ----------------------------
-  function getTypeByName(name) {
-    var i, len;
-    for (i = 0, len = types.length; i < len; i++) {
-      if (types[i].name === name) {
-        return types[i];
+  function createType(name, config) {
+    var messages = {}, messageName;
+    for (messageName in config.messages) {
+      if (config.messages.hasOwnProperty(messageName) && typeof config.messages[messageName] === 'function') {
+        messages[messageName] = config.messages[messageName];
       }
     }
-    return null;
+    return {
+      name: name,
+      messages: messages
+    };
   }
 
-  function createType(name, config) {
-    var type = getTypeByName(name);
-    if (type) {
+  function addType(name, config) {
+    if (getItemByProp(types, 'name', name)) {
       throw new Error('Process type already exists: ' + name);
     }
-    var messages = {};
-    var prop;
-    for (prop in config.messages) {
-      if (config.messages.hasOwnProperty(prop) && typeof config.messages[prop] === 'function') {
-        messages[prop] = config.messages[prop];
-      }
-    }
-    types.push({ name: name, messages: messages });
+    types.push(createType(name, config));
   }
 
   // Processes
   // ----------------------------
-  function getProcessByName(name) {
-    var i, len;
-    for (i = 0, len = processes.length; i < len; i++) {
-      if (processes[i].name === name) {
-        return processes[i];
-      }
+  function createProcess(type, name) {
+    var thisProcess;
+
+    function getName() {
+      return name;
     }
-    return null;
+
+    function getType() {
+      return type.name;
+    }
+
+    function getManager() {
+      return manager;
+    }
+
+    function getCurrentSenderName() {
+      return currentSender ? currentSender.name : null;
+    }
+
+    function send(receiverName, message) {
+      var receiver = getItemByProp(processes, 'name', receiverName);
+      if (!receiver) {
+        throw new Error('Receiver process not found: ' + receiverName);
+      }
+      var args = getItemsFromIndex(arguments, 2);
+      sentEntries.push({ sender: thisProcess, receiver: receiver, message: message, args: args });
+      requestMessageHandler();
+    }
+
+    function broadcast(message) {
+      var args = getItemsFromIndex(arguments, 1);
+      sentEntries.push({ sender: thisProcess, receiver: null, message: message, args: args });
+      requestMessageHandler();
+    }
+    
+    return thisProcess = {
+      name: name,
+      type: type,
+      public: {
+        getName: getName,
+        getType: getType,
+        getManager: getManager,
+        getCurrentSenderName: getCurrentSenderName,
+        send: send,
+        broadcast: broadcast
+      }
+    };
   }
 
   function spawn(typeName, name) {
-    var type = getTypeByName(typeName);
+    var type = getItemByProp(types, 'name', typeName);
     if (!type) {
       throw new Error('Process type not found: ' + typeName);
     }
@@ -56,43 +90,12 @@ function createProcessManager() {
       do {
         name = typeName + i;
         i++;
-      } while (getProcessByName(name));
+      } while (getItemByProp(processes, 'name', name));
     }
-    var thisProcess;
-    processes.push(thisProcess = {
-      name: name,
-      type: type,
-      instance: {
-        getManager: function () {
-          return manager;
-        },
-        getName: function () {
-          return thisProcess.name;
-        },
-        send: function (receiverName, message) {
-          var receiver = getProcessByName(receiverName);
-          if (!receiver) {
-            throw new Error('Receiver process not found: ' + receiverName);
-          }
-          var args = [];
-          var i, len;
-          for (i = 2, len = arguments.length; i < len; i++) {
-            args.push(arguments[i]);
-          }
-          sentEntries.push({ sender: thisProcess, receiver: receiver, message: message, args: args });
-          requestMessageHandler();
-        },
-        broadcast: function (message) {
-          var args = [];
-          var i, len;
-          for (i = 1, len = arguments.length; i < len; i++) {
-            args.push(arguments[i]);
-          }
-          sentEntries.push({ sender: thisProcess, receiver: null, message: message, args: args });
-          requestMessageHandler();
-        }
-      }    
-    });
+    var process = createProcess(type, name);
+    processes.push(process);
+    send(process.name, 'boot');
+    return process.name;
   }
 
   // Messages
@@ -112,46 +115,31 @@ function createProcessManager() {
       var entry = sentEntries.shift();
       currentSender = entry.sender;
       var message = entry.message;
-      if (entry.receiver === null) {
-        var i, len;
-        for (i = 0, len = processes.length; i < len; i++) {
-          var receiver = processes[i];
-          var handler = receiver.type.messages[message];
-          if (handler) {
-            handler.apply(receiver.instance, entry.args || []);
-          }
+      var receivers = entry.receiver === null ? processes : [entry.receiver];
+      var i, len;
+      for (i = 0, len = receivers.length; i < len; i++) {
+        var receiver = receivers[i];
+        var messageHandler = receiver.type.messages[message];
+        if (typeof messageHandler === 'function') {
+          messageHandler.apply(receiver.public, entry.args);
         }
-        continue;
-      }
-      var receiver = entry.receiver; 
-      var handler = receiver.type.messages[message];
-      if (handler) {
-        handler.apply(receiver.instance, entry.args || []);
       }
     }
     currentSender = null;
   }
 
   function send(receiverName, message) {
-    var receiver = getProcessByName(receiverName);
+    var receiver = getItemByProp(processes, 'name', receiverName);
     if (!receiver) {
       throw new Error('Receiver process not found: ' + receiverName);
     }
-    var args = [];
-    var i, len;
-    for (i = 2, len = arguments.length; i < len; i++) {
-      args.push(arguments[i]);
-    }
+    var args = getItemsFromIndex(arguments, 2);
     sentEntries.push({ sender: null, receiver: receiver, message: message, args: args });
     requestMessageHandler();
   }
 
   function broadcast(message) {
-    var args = [];
-    var i, len;
-    for (i = 1, len = arguments.length; i < len; i++) {
-      args.push(arguments[i]);
-    }
+    var args = getItemsFromIndex(arguments, 1);
     sentEntries.push({ sender: null, receiver: null, message: message, args: args });
     requestMessageHandler();
   }
@@ -160,24 +148,60 @@ function createProcessManager() {
     return currentSender ? currentSender.name : null;
   }
 
+  // Utils
+  // ----------------------------
+  function getItemByProp(list, prop, value) {
+    var i, len;
+    for (i = 0, len = list.length; i < len; i++) {
+      if (list[i][prop] === value) {
+        return list[i];
+      }
+    }
+    return null;
+  }
+
+  function getItemsFromIndex(list, index) {
+    var result = [];
+    var i, len;
+    for (i = index, len = list.length; i < len; i++) {
+      result.push(list[i]);
+    }
+    return result;
+  }
+
   return manager = {
-    createType: createType,
+    addType: addType,
     spawn: spawn,
     send: send,
-    broadcast: broadcast,
-    getCurrentSenderName: getCurrentSenderName
+    broadcast: broadcast
   };
 }
 
 var mgr = createProcessManager();
-mgr.createType('example', {
+mgr.addType('example', {
   messages: {
     say: function (something) {
       console.log(this.getName(), something);
     },
   },
 });
-mgr.spawn('example', 'process1');
-mgr.spawn('example', 'process2');
-mgr.send('process1', 'say', 'Hello from process1');
-mgr.send('process2', 'say', 'Hello from process2');
+mgr.addType('example2', {
+  messages: {
+    boot: function (something) {
+      var self = this;
+      setInterval(function () {
+        self.broadcast('say', 'Tick from ' + self.getName());
+      }, 2000);
+    }
+  },
+});
+
+mgr.spawn('example');
+mgr.spawn('example');
+mgr.spawn('example');
+mgr.spawn('example');
+mgr.spawn('example');
+mgr.spawn('example');
+mgr.spawn('example');
+mgr.spawn('example');
+mgr.spawn('example2');
